fea46f2c3fb75397f5974473574bded55b5821fb
Victoria B <viktorija.bourassa@gmail.com>
2019-01-22 12:12:07 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,36 @@
+class Song
+  attr_accessor :name, :genre, :artist
+  @@count = 0
+  @@artists ||= []
+  @@genres ||= []
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count+=1
+    @@artists << artists
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def artists
+    @@artists
+  end
+
+  def self.genres
+    @@genres
+  end
+
+  def genre_count
+    genres.each do |k, v|
+end
+  end
+
+  def artist_count
+  end
+
+
+end
